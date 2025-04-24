e6532638d62ba280c488b64082d2d1cca3cb45bc
Pushpender Singh <pushpender7291rajput@gmail.com>
2025-04-24 14:32:28 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -52,7 +52,8 @@ export const register = async (req , res) => {
         res.cookie("token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production", // ✔️ auto true on Vercel
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // ✔️ lax for local, none for vercel
+          // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // ✔️ lax for local, none for vercel
+          sameSite: "none", // ✔️ lax for local, none for vercel
           maxAge: 7 * 24 * 60 * 60 * 1000, // ✔️ 7 days
         });
 
@@ -141,7 +142,8 @@ export const login = async (req , res) => {
         res.cookie("token", token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production", // ✔️ auto true on Vercel
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // ✔️ lax for local, none for vercel
+          // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // ✔️ lax for local, none for vercel
+          sameSite: "none", // ✔️ lax for local, none for vercel
           maxAge: 7 * 24 * 60 * 60 * 1000, // ✔️ 7 days
         });
         // finally 
